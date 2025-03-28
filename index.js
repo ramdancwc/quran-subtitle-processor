597c74f543fa6cd3a60a179b528d2e13b09f8286
ramdancwc <ramdancwc@gmail.com>
2025-03-28 20:12:56 +0000

    simplerUpdate index.js

--- index.js
+++ index.js
@@ -196,40 +196,20 @@ function formatSRTTime(seconds) {
 
 
 // Create MediaConvert job parameters
+// Create MediaConvert job parameters
 function createMediaConvertParams(videoUrl, srtKey, outputKey, preferences) {
   return {
     Role: process.env.MEDIACONVERT_ROLE_ARN,
     Settings: {
-      InputGroups: [],
-      AdAvailOffset: 0,
       Inputs: [
         {
-          AudioSelectors: {
-            "Audio Selector 1": {
-              Offset: 0,
-              DefaultSelection: "DEFAULT",
-              ProgramSelection: 1,
-            }
-          },
-          VideoSelector: {
-            ColorSpace: "FOLLOW",
-            Rotate: "AUTO",
-            AlphaBehavior: "DISCARD"
-          },
-          FilterEnable: "AUTO",
-          PsiControl: "USE_PSI",
-          FilterStrength: 0,
-          DeblockFilter: "DISABLED",
-          DenoiseFilter: "DISABLED",
-          TimecodeSource: "EMBEDDED",
           FileInput: videoUrl,
           CaptionSelectors: {
-            "Captions Selector 1": {
+            "Captions": {
               SourceSettings: {
                 SourceType: "SRT",
                 FileSourceSettings: {
-                  SourceFile: `s3://${process.env.S3_BUCKET}/${srtKey}`,
-                  TimeDelta: 0
+                  SourceFile: `s3://${process.env.S3_BUCKET}/${srtKey}`
                 }
               }
             }
@@ -238,7 +218,6 @@ function createMediaConvertParams(videoUrl, srtKey, outputKey, preferences) {
       ],
       OutputGroups: [
         {
-          Name: "File Group",
           OutputGroupSettings: {
             Type: "FILE_GROUP_SETTINGS",
             FileGroupSettings: {
@@ -248,81 +227,32 @@ function createMediaConvertParams(videoUrl, srtKey, outputKey, preferences) {
           Outputs: [
             {
               VideoDescription: {
-                ScalingBehavior: "DEFAULT",
-                TimecodeInsertion: "DISABLED",
-                AntiAlias: "ENABLED",
-                Sharpness: 50,
                 CodecSettings: {
                   Codec: "H_264",
                   H264Settings: {
-                    InterlaceMode: "PROGRESSIVE",
-                    NumberReferenceFrames: 3,
-                    Syntax: "DEFAULT",
-                    Softness: 0,
-                    GopClosedCadence: 1,
-                    GopSize: 90,
-                    Slices: 1,
-                    GopBReference: "DISABLED",
-                    SlowPal: "DISABLED",
-                    SpatialAdaptiveQuantization: "ENABLED",
-                    TemporalAdaptiveQuantization: "ENABLED",
-                    FlickerAdaptiveQuantization: "DISABLED",
-                    EntropyEncoding: "CABAC",
-                    Bitrate: 5000000,
-                    FramerateControl: "INITIALIZE_FROM_SOURCE",
                     RateControlMode: "CBR",
-                    CodecProfile: "MAIN",
-                    Telecine: "NONE",
-                    MinIInterval: 0,
-                    AdaptiveQuantization: "HIGH",
-                    CodecLevel: "AUTO",
-                    FieldEncoding: "PAFF",
-                    SceneChangeDetect: "ENABLED",
-                    QualityTuningLevel: "SINGLE_PASS",
-                    FramerateConversionAlgorithm: "DUPLICATE_DROP",
-                    UnregisteredSeiTimecode: "DISABLED",
-                    GopSizeUnits: "FRAMES",
-                    ParControl: "INITIALIZE_FROM_SOURCE",
-                    NumberBFramesBetweenReferenceFrames: 2,
-                    RepeatPps: "DISABLED",
-                    DynamicSubGop: "STATIC"
+                    Bitrate: 5000000
                   }
-                },
-                AfdSignaling: "NONE",
-                DropFrameTimecode: "ENABLED",
-                RespondToAfd: "NONE",
-                ColorMetadata: "INSERT"
+                }
               },
               AudioDescriptions: [
                 {
-                  AudioTypeControl: "FOLLOW_INPUT",
                   CodecSettings: {
                     Codec: "AAC",
                     AacSettings: {
-                      AudioDescriptionBroadcasterMix: "NORMAL",
                       Bitrate: 96000,
-                      RateControlMode: "CBR",
-                      CodecProfile: "LC",
                       CodingMode: "CODING_MODE_2_0",
-                      RawFormat: "NONE",
-                      SampleRate: 48000,
-                      Specification: "MPEG4"
+                      SampleRate: 48000
                     }
-                  },
-                  LanguageCodeControl: "FOLLOW_INPUT"
+                  }
                 }
               ],
               ContainerSettings: {
-                Container: "MP4",
-                Mp4Settings: {
-                  CslgAtom: "INCLUDE",
-                  FreeSpaceBox: "EXCLUDE",
-                  MoovPlacement: "PROGRESSIVE_DOWNLOAD"
-                }
+                Container: "MP4"
               },
               CaptionDescriptions: [
                 {
-                  CaptionSelectorName: "Captions Selector 1",
+                  CaptionSelectorName: "Captions",
                   DestinationSettings: {
                     DestinationType: "BURN_IN"
                   }
